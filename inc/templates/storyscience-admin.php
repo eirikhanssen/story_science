1af97e2dc6bd535c3ab9aef679cefbfb87e592d1
eirikhanssen <eirikhanssen79@gmail.com>
2017-02-20 17:33:52 +0100

    store and retrieve the settings

--- inc/templates/storyscience-admin.php
+++ inc/templates/storyscience-admin.php
@@ -1,5 +1,6 @@
 <h1>Storyscience Theme Options</h1>
-<form method="post" action="">
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
 	<?php settings_fields('storyscience-settings-group');?>
 	<?php do_settings_sections('storyscience_settings') ?>
 	<?php submit_button(); ?>
